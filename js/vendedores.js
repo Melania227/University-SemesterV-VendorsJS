86b8f0089ea87e3339e16c837ab36d3742fbd3d4
Melania Morales <melaniacrc@gmail.com>
2021-05-28 12:03:31 -0600

    U: Data addition to table & datatable config

--- js/vendedores.js
+++ js/vendedores.js
@@ -1,30 +1,64 @@
+/* FIREBASE */
 // en estas const para manejar facilemtne la base de datos.
 const database = firebase.database();
 // referencia a la collection test_col para utilizar las funciones sobre esta colección
 const rootRef = database.ref('/');
-//
+
+/* WORKING WITH JSON INFORMATION */
+
+$(document).ready(function(){
+  datatablePropierties();
+});
+
 let jsonData = [];
 
 // se agrega el listener al botón remove
 function showData (){
-    let html = '<ul>';
+  $('#vendedoresTables').DataTable().destroy();
+  
+  let htmlTable = '';
 
-    jsonData.forEach(element => {
-        html += '<li>'+ element.infoResult.data[0].slpName +'</li>'    
-        
-    });
-    html += '</ul>';
-    
-    
-    document.getElementById("demo").innerHTML = html;
-    console.log(html);
-    console.log(document.getElementById('demo'));
+  jsonData.forEach(vendedor => {
+      htmlTable += '<tr vendedorID = "' + vendedor.infoResult.data[0].username + '">';
+      htmlTable += '<td class="text-truncate text-center">' + vendedor.infoResult.data[0].slpName + '</td>';
+      htmlTable += '<td class="text-truncate text-center">' + vendedor.infoResult.data[0].sale + '</td>';
+      htmlTable += '<td class="text-truncate text-center">' + vendedor.infoResult.data[0].budget + '</td>';
+      htmlTable += '<td class="text-truncate text-center">' + (vendedor.infoResult.data[0].sale - vendedor.infoResult.data[0].budget) + '</td>';
+      htmlTable += '<td class="text-truncate text-center">' + (vendedor.infoResult.data[0].sale / vendedor.infoResult.data[0].budget) + '</td>';
+      htmlTable += '<td class="text-truncate text-center"><i class="fas fa-paper-plane"></i></td>';
+      htmlTable += '</tr>';
+  });    
+  document.getElementById('vendedorTableBodyID').innerHTML = htmlTable;
+  /* $('#vendedorTableBodyID').html = htmlTable; */
 }
+
 function getData (){
- // once() method
-    rootRef.on('value',(snap)=>{
+  rootRef.on('value',(snap)=>{
     jsonData = snap.val();
     console.log(jsonData);
   });
 }
+
+function datatablePropierties(){
+  $('#vendedorTableID').DataTable({
+    "bSort": true,
+    "paging":true,
+    
+    "language": {
+      "search": "Buscar",
+      "paginate":{
+        "show": "Mostrando",
+        "first": "Primer",
+        "previous": "Anterior",
+        "next": "Siguiente",
+        "last": "Ultimo"
+      },
+      "info": "Mostrando página _PAGE_ de _PAGES_",
+      "infoEmpty": "No hay datos",
+      "emptyTable": "No hay datos por el momento"
+    }
+  });
+}
+
+
 
