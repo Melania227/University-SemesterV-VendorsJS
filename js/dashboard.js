443297b517ec0494722bc687596bbc2181dd4765
Melania Morales <melaniacrc@gmail.com>
2021-05-28 21:40:05 -0600

    C: Firebase search by user ID

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,2 +1,29 @@
-let idGuardadoEnLocal = localStorage.getItem("idUser");
-document.getElementById('prueba').innerHTML = idGuardadoEnLocal;
+/* FIREBASE */
+// en estas const para manejar facilemtne la base de datos.
+const database = firebase.database();
+// referencia a la collection test_col para utilizar las funciones sobre esta colección
+const rootRef = database.ref('/');
+
+/* WORKING WITH JSON INFORMATION */
+
+$(document).ready(function(){
+  //datatablePropierties();
+});
+
+function getData (){
+    rootRef.on('value',(snap)=>{
+      jsonData = snap.val();
+      console.log(jsonData);
+    });
+  }
+
+let jsonData = [];
+
+// se agrega el listener al botón remove
+function showData (){
+    let idGuardadoEnLocal = localStorage.getItem("idUser");
+    let dataUser = jsonData[idGuardadoEnLocal];
+    console.log(dataUser);
+    //document.getElementById('prueba').innerHTML = idGuardadoEnLocal;
+}
+
